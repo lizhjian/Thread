bfddae8a6d6eacda69219685520f5bb04099493b
lizhijian <907257608@qq.com>
2018-03-20 07:07:41 +0800

    增加同步代码块

--- src/com/test04/TicketDemo.java
+++ src/com/test04/TicketDemo.java
@@ -1,5 +1,7 @@
 package com.test04;
 
+import static java.lang.Thread.sleep;
+
 /**
  * 实现接口的方式
  * 1、定义类实现Runnable解扣子
@@ -8,15 +10,35 @@ package com.test04;
  * 4、将Runnable接口的子类对象作为实际参数传递给Thread类的构造函数
  * 5、调用Thread类的start方法开启线程并调用Runnable接口子类的run方法
  *
+ * 实现方式好处：避免了单线程的局限性
+ * 在定义线程时，建议使用实现接口方式
+ *
+ * 通过分析发现  打印出0 等错票
+ * 多线程运行出现了安全问题
+ * 问题的原因：
+ * 当多条语句在操作同一个多线程共享数据时，一个线程对多条语句只执行了一部分，
+ * 还没有执行完，另一个线程参与进来执行
  */
-class  Ticket implements Runnable{
+class  Ticket  implements Runnable {
     private int tick = 100;
+    Object object = new Object();
     @Override
-    public void run() {
+    public  void run() {
         while (true){
-            if(tick>0){
-                System.out.println(Thread.currentThread().getName()+"..sale"+tick--);
+
+            synchronized(object){
+                if(tick>0){
+                    try{
+                        Thread.sleep(10);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(Thread.currentThread().getName()+"..sale---"+tick--);
+                }
             }
+
+
         }
     }
 }
@@ -27,11 +49,9 @@ public class TicketDemo {
         Ticket t = new Ticket();
      Thread t1 = new Thread( t);
      Thread t2 = new Thread( t);
-     Thread t3 = new Thread( t);
-     Thread t4 = new Thread( t);
+
      t1.start();
      t2.start();
-     t3.start();
-     t4.start();
+
     }
 }
