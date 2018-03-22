64a64405af2ebe50c4413bb74b3797da792926cf
lizhijian <907257608@qq.com>
2018-03-22 09:26:50 +0800

    join方法

---
+++ src/com/JoinDemo/JoinDemo.java
@@ -0,0 +1,33 @@
+package com.JoinDemo;
+
+/**
+ * join
+ * 当A线程执行到B线程的join方法时，A就会等待，等B线程都执行完，A才会执行
+ * join可以用来临时加入线程执行
+ *
+ */
+class  Demo implements Runnable{
+    @Override
+    public void run() {
+        for (int x = 0;x<50;x++){
+            System.out.println(Thread.currentThread().getName()+"......"+x);
+        }
+    }
+}
+
+
+public class JoinDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Demo demo = new Demo();
+        Thread t1 = new Thread(demo);
+        Thread t2 = new Thread(demo);
+        t2.start();
+        t1.start();
+        t1.join();  //t1抢夺cpu执行权，main冻结停止，t1 t2 交替执行，等t1结束后main活过来
+
+        for(int x =0;x<80;x++){
+            System.out.println("main......"+x);
+        }
+        System.out.println("over");
+    }
+}
